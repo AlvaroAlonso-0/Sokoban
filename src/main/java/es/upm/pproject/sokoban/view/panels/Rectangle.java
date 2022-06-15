a5c815a98d3368214e37f5af2124555d56ff74ea
ALONSO SIRERA, RAFAEL <rafael.alonso.sirera@alumnos.upm.es>
2022-06-15 20:43:06 +0200

    Rectangle class reviewed

--- src/main/java/es/upm/pproject/sokoban/view/panels/Rectangle.java
+++ src/main/java/es/upm/pproject/sokoban/view/panels/Rectangle.java
@@ -4,8 +4,19 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
+/**
+* Class that represents a Rectangle it will be used to draw borders surrounding the game board
+* @author Idir Carlos Aliane Crespo
+* @version 1.1
+* @since 15/06/2022
+*/
 public class Rectangle extends JPanel{
 
+    /**
+     * Rectangle constructor
+     * @param width Rectangle width 
+     * @param height Rectangle height
+     */
     public Rectangle(int width, int height) {
         Dimension size = new Dimension(width,height);
         setPreferredSize(size);
